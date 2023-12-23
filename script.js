b004eb1d62aff5ee8106a28092df5dd25a7e428a
Irakly_Pavliashvili <ip20061009@gmail.com>
2023-12-23 09:18:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let x = 100;
+if (x < 50){
+    console.log("less than 50");
+} else if (x > 20){
+    console.log("greater than 20");
+}else {
+    console.log("error");
+}
+
+let name = "mariam"
+if (name === "mariam"){
+    console.log(true);
+} else{
+    console.log(false);
+}
+
+let name1 = name === "mariam" ? true : false
+
+switch (name){
+    case "mariam":
+        console.log(true);
+        break;
+    default:
+        console.log(false);
+}
+
+let num = Number(prompt("ENTER NUMBER"));
+if (num % 2 === 0) {
+    console.log("even number");
+} else {
+    console.log("odd number");
+}
+
